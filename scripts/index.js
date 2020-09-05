92d7d6a246f0f2adf123d1fdaaf4c73a320591cc
srujanjill <srujanjilla15@gmail.com>
2020-09-05 23:50:32 +0530

    create new guide

--- scripts/index.js
+++ scripts/index.js
@@ -1,26 +1,25 @@
 // DOM elements
-const guideList = document.querySelector('.guides');
-const loggedOutLinks = document.querySelectorAll('.logged-out');
-const loggedInLinks = document.querySelectorAll('.logged-in');
+const guideList = document.querySelector(".guides");
+const loggedOutLinks = document.querySelectorAll(".logged-out");
+const loggedInLinks = document.querySelectorAll(".logged-in");
 
 const setupUI = (user) => {
   if (user) {
     // toggle user UI elements
-    loggedInLinks.forEach(item => item.style.display = 'block');
-    loggedOutLinks.forEach(item => item.style.display = 'none');
+    loggedInLinks.forEach((item) => (item.style.display = "block"));
+    loggedOutLinks.forEach((item) => (item.style.display = "none"));
   } else {
     // toggle user elements
-    loggedInLinks.forEach(item => item.style.display = 'none');
-    loggedOutLinks.forEach(item => item.style.display = 'block');
+    loggedInLinks.forEach((item) => (item.style.display = "none"));
+    loggedOutLinks.forEach((item) => (item.style.display = "block"));
   }
 };
 
 // setup guides
 const setupGuides = (data) => {
-
   if (data.length) {
-    let html = '';
-    data.forEach(doc => {
+    let html = "";
+    data.forEach((doc) => {
       const guide = doc.data();
       const li = `
         <li>
@@ -30,21 +29,17 @@ const setupGuides = (data) => {
       `;
       html += li;
     });
-    guideList.innerHTML = html
+    guideList.innerHTML = html;
   } else {
     guideList.innerHTML = '<h5 class="center-align">Login to view guides</h5>';
   }
-  
-
 };
 
 // setup materialize components
-document.addEventListener('DOMContentLoaded', function() {
-
-  var modals = document.querySelectorAll('.modal');
+document.addEventListener("DOMContentLoaded", function () {
+  var modals = document.querySelectorAll(".modal");
   M.Modal.init(modals);
 
-  var items = document.querySelectorAll('.collapsible');
+  var items = document.querySelectorAll(".collapsible");
   M.Collapsible.init(items);
-
 });
