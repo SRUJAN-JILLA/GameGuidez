3eab302c3473be1d5a9ca4a4c28713009af71ae5
srujanjill <srujanjilla15@gmail.com>
2020-09-05 12:08:33 +0530

    Logout setup

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,18 +1,26 @@
 // signup
-const signupForm = document.querySelector("#signup-form");
-signupForm.addEventListener("submit", (e) => {
+const signupForm = document.querySelector('#signup-form');
+signupForm.addEventListener('submit', (e) => {
   e.preventDefault();
-
+  
   // get user info
-  const email = signupForm["signup-email"].value;
-  const password = signupForm["signup-password"].value;
+  const email = signupForm['signup-email'].value;
+  const password = signupForm['signup-password'].value;
 
   // sign up the user
-  auth.createUserWithEmailAndPassword(email, password).then((cred) => {
-    console.log(cred.user);
+  auth.createUserWithEmailAndPassword(email, password).then(cred => {
     // close the signup modal & reset form
-    const modal = document.querySelector("#modal-signup");
+    const modal = document.querySelector('#modal-signup');
     M.Modal.getInstance(modal).close();
     signupForm.reset();
   });
+});
+
+// logout
+const logout = document.querySelector('#logout');
+logout.addEventListener('click', (e) => {
+  e.preventDefault();
+  auth.signOut().then(() => {
+    console.log('user signed out');
+  })
 });
