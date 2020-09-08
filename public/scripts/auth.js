69f58b347a39ec89c5bcbf34ab64adbf23de6d72
srujanjill <srujanjilla15@gmail.com>
2020-09-08 14:39:49 +0530

    Catching Auth Errors

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -71,6 +71,10 @@ signupForm.addEventListener("submit", (e) => {
       const modal = document.querySelector("#modal-signup");
       M.Modal.getInstance(modal).close();
       signupForm.reset();
+      signupForm.querySelector(".error").innerHTML = "";
+    })
+    .catch((err) => {
+      signupForm.querySelector(".error").innerHTML = err.message;
     });
 });
 
@@ -91,10 +95,16 @@ loginForm.addEventListener("submit", (e) => {
   const password = loginForm["login-password"].value;
 
   // log the user in
-  auth.signInWithEmailAndPassword(email, password).then((cred) => {
-    // close the signup modal & reset form
-    const modal = document.querySelector("#modal-login");
-    M.Modal.getInstance(modal).close();
-    loginForm.reset();
-  });
+  auth
+    .signInWithEmailAndPassword(email, password)
+    .then((cred) => {
+      // close the signup modal & reset form
+      const modal = document.querySelector("#modal-login");
+      M.Modal.getInstance(modal).close();
+      loginForm.reset();
+      loginForm.querySelector(".error").innerHTML = "";
+    })
+    .catch((err) => {
+      loginForm.querySelector(".error").innerHTML = err.message;
+    });
 });
