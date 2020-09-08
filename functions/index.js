c1bfef40d3769a680f580ffbbea42a31762f600c
srujanjill <srujanjilla15@gmail.com>
2020-09-08 14:22:36 +0530

    securing cloud functions

--- functions/index.js
+++ functions/index.js
@@ -1,18 +1,27 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
 admin.initializeApp();
 
 exports.addAdminRole = functions.https.onCall((data, context) => {
+  // check request is made by an admin
+  if (context.auth.token.admin !== true) {
+    return { error: "Only admins can add other admins" };
+  }
   // get user and add admin custom claim
-  return admin.auth().getUserByEmail(data.email).then(user => {
-    return admin.auth().setCustomUserClaims(user.uid, {
-      admin: true
+  return admin
+    .auth()
+    .getUserByEmail(data.email)
+    .then((user) => {
+      return admin.auth().setCustomUserClaims(user.uid, {
+        admin: true,
+      });
     })
-  }).then(() => {
-    return {
-      message: `Success! ${data.email} has been made an admin.`
-    }
-  }).catch(err => {
-    return err;
-  });
+    .then(() => {
+      return {
+        message: `Success! ${data.email} has been made an admin.`,
+      };
+    })
+    .catch((err) => {
+      return err;
+    });
 });
